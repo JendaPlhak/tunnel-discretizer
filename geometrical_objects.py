Fix radius calculation.

geometrical_objects.py
@@ -1,3 +1,5 @@
+from linalg import *
+
 class Disk:
     def __init__(self, center, normal, radius):
         self.center = center
@@ -14,6 +16,16 @@ class Disk:
                 radius=self.radius, 
                 thickness=0.01)
 
+class Plane:
+    def __init__(self, point, normal):
+        self.point  = point
+        self.normal = normal
+    def contains(self, point):
+        diff = np.dot(self.normal, self.point) - np.dot(self.normal, point)
+        return abs(diff) < f_error
+    def intersection_line(self, line):
+        return line.intersection_plane(self)
+
 class Sphere:
     def __init__(self, center, radius):
         self.center = center
@@ -22,10 +34,41 @@ class Sphere:
         packer  = lambda c : tuple([c[0], c[1], c[2]])
         return {"center" : packer(self.center), 
                 "normal" : packer(self.normal)}
+    def intersection_line(self, line):
+        return line.intersection_sphere(self)
 
 class Line:
-    def __init__(self, dir_, point):
+    def __init__(self, point, dir_):
         self.dir   = dir_
         self.point = point
     def get_line_point(self, t):
         return self.point + t * self.dir
+    def intersection_sphere(self, sphere):
+        return get_intersection_line_sphere(sphere, self)
+    def intersection_plane(self, plane):
+        # First get vectors for plane parametric form
+        # print "plane", plane.point, plane.normal
+        # print "line", self.point, self.dir
+        v1 = normalize(null_space(np.array([plane.normal, null_vec, null_vec])))
+        v2 = normalize(null_space(np.array([plane.normal, v1, null_vec])))
+        assert is_3D_basis(plane.normal, v1, v2)
+
+        r_side = np.transpose(np.array([plane.point - self.point]))
+        l_side = np.transpose(np.array([self.dir, v1, v2]))
+        # print "left side:\n", l_side
+        # print "right side:\n", r_side
+
+        l_side_inv = np.linalg.inv(l_side)
+        parameters = np.dot(l_side_inv, r_side)
+
+        # print "Result:"
+        # print "parameters:\n", parameters
+        # print "dir:\n", self.dir
+        # print self.get_line_point(parameters[0])
+        intersection = self.get_line_point(parameters[0])
+        assert plane.contains(intersection)
+        return intersection
+
+
+
+

readtunel.py
@@ -3,124 +3,42 @@
 """Tunnel generator.
 
 Usage:
-  naval_fate.py (-f | --file) <filename>
+  readtunel.py (-f | --file) <filename> (-d | --draw)
+  readtunel.py (-f | --file) <filename>
 
 Options:
   -h --help     Show this screen.
   -f --file     File containing information about tunnel in molecule.
+  -d --draw     Draw scenario into picture using vpython
 
 """
 
+from visual import *
 import visual as vs
 import numpy as np
 import sys
+from decimal import *
 import math
 import json
 from docopt import docopt
-from geometrical_objects import Disk, Sphere, Line 
+from geometrical_objects import Disk, Sphere, Plane, Line 
+from linalg import *
 
-f_error = 0.000001
-
-null_vec = np.array([0,0,0])
-
-def point_dist(point1, point2):
-    return abs(np.linalg.norm(point2 - point1))
-
-def normalize(v):
-    norm = np.linalg.norm(v)
-    if norm == 0: 
-       return v
-    return v / norm
-
-def null_space(A, eps=1e-15):
-    u, s, vh = np.linalg.svd(A)
-    null_space = np.compress(s <= eps, vh, axis=0)
-    return null_space[0]
-
-# For two disks calculate vector that realizes radius of segment that emerges by 
-# projecting disk into the plain determined by normals of disks d1 and d2.
-def get_radius_vectors(d1, d2):
-    # Get normal vector of plain given by normal vectors of disks.
-    normal = null_space(np.array([d1.normal, d2.normal, null_vec]))
-    # print "Normal:\n{}".format(normal)
-    # Calculate directions of segments created by projection to the plain.
-    seg_dir1 = null_space(np.array([d1.normal, normal, null_vec]))
-    seg_dir2 = null_space(np.array([d2.normal, normal, null_vec]))
-    # Get radius vector
-    seg_dir1 = normalize(seg_dir1) * d1.radius
-    seg_dir2 = normalize(seg_dir2) * d2.radius
-
-    return seg_dir1, seg_dir2
-
-
-# projection distance of two discs
-def disk_dist(d1, d2):
-    if (d1.normal == d2.normal).all():
-        return np.linalg.norm(d1.center - d2.center)
-    else:
-        # get radius vectors
-        seg_dir1, seg_dir2 = get_radius_vectors(d1, d2)
-        # print "Radius vector 1:\n{}".format(seg_dir1)
-        # print "Radius vector 2:\n{}".format(seg_dir2)
-
-        # for two sets of points calculate distances between pairs
-        def distances(v_1, v_2):
-            return [np.linalg.norm(x[0] - x[1]) for x in zip(v_1, v_2)]
-
-        seg_1_verts = [d1.center + seg_dir1, d1.center - seg_dir1]
-        seg_2_verts = [d2.center + seg_dir2, d2.center - seg_dir2]
-
-        dists_1 = distances(seg_1_verts, seg_2_verts)
-        dists_2 = distances(list(reversed(seg_1_verts)), seg_2_verts)
-
-        if sum(dists_1) < sum(dists_2):
-            return max(dists_1)
-        else:
-            return max(dists_2)
-
-        # Calculate transformation matrix from standard basis to 
-        # (d1.normal, d2.normal, normal).
-        print "Transformation matrix from beta to standard:\n{}".format(
-            np.array([d1.normal, d2.normal, normal]))
-        A     = np.array([d1.normal, d2.normal, normal])
-        A_inv = np.linalg.inv(np.array([d1.normal, d2.normal, normal]))
-        # print dot(d1.normal, A_inv) 
-        # print dot(d2.normal, A_inv) 
-        # print dot(normal, A_inv) 
-        # Because 'normal' vector is perpendicular to disk normals, the third
-        # coordinate of segment directions will be always zero in this basis.
-        seg_dir1_proj = dot(seg_dir1, A_inv) 
-        seg_dir2_proj = dot(seg_dir2, A_inv) 
-
-# Calculate points of intersection between line and sphere.
-# for reference see https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
-def get_intersection_line_sphere(sphere, line):
-    line_dir = normalize(line.dir)
-    r   = sphere.radius
-    vec = line.point - sphere.center
-
-    discrim = np.dot(line_dir, vec)**2 \
-        - np.dot(vec, vec) \
-        + r**2
-
-    if discrim < 0.0:
-        return []
-
-    inter_points = []
-    for i in xrange(2):
-        # d is distance from starting point
-        d = - np.dot(line_dir, vec) + (-1)**i * math.sqrt(discrim)
-        inter_points.append(line.point + d * line_dir)
-
-    return inter_points
         
-
 def get_radius(normal, center, tunnel):
-    line_dir = normalize(null_space(np.array([normal, null_vec, null_vec])))
-    line     = Line(line_dir, center)
     r = sys.float_info.min
     for sphere in tunnel:
-        inter_points = get_intersection_line_sphere(sphere, line)
+        perpendicular_line = Line(sphere.center, normal)
+        disk_plane = Plane(center, normal)
+        cap_center = perpendicular_line.intersection_plane(disk_plane)
+
+        if (cap_center == center).all():
+            line_dir = normalize(null_space(np.array([normal, null_vec, null_vec])))
+            assert abs(np.dot(line_dir, normal)) < f_error
+        else:
+            line_dir = cap_center - center
+        line = Line(center, line_dir)
+        inter_points = sphere.intersection_line(line)
         if len(inter_points) == 2:
             v1 = inter_points[0] - line.point
             v2 = inter_points[1] - line.point
@@ -130,7 +48,7 @@ def get_radius(normal, center, tunnel):
             elif np.dot(v1, v2) < 0:
                 r1 = np.linalg.norm(v1)
                 r2 = np.linalg.norm(v2)
-                r = max(r, min(r1, r2))
+                r = max(r, max(r1, r2))
             else:
                 raise ValueError("One of vectors is null")
         else:
@@ -161,8 +79,8 @@ def normal_in_plane(n, v):
     assert np.dot(u, v) < 0.00001
     return u
 
-# For two given points and vector determining plain, calculate disk, that is 
-# perpendicular to that plain, and its vertices are point1 and point2
+# For two given points and vector determining plan, calculate disk, that is 
+# perpendicular to that plan, and its vertices are point1 and point2
 def get_new_disk_points(point1, point2, n):
     center = (point1 + point2) / 2.
     # print "Point 1: {}, Point 2: {}".format(point1, point2)
@@ -171,7 +89,7 @@ def get_new_disk_points(point1, point2, n):
     # print "New normal: {}".format(normal)
     return Disk(center, normal, point_dist(point1, point2) / 2.)
 
-def shift_new_disk(new_disk, prev_disk):
+def shift_new_disk(new_disk, prev_disk, tunnel):
     new_dir, prev_dir = get_radius_vectors(new_disk, prev_disk)
     # Get proj. disk vertices
     new_vert_1 = new_disk.center + new_dir
@@ -183,8 +101,6 @@ def shift_new_disk(new_disk, prev_disk):
     v1 = new_vert_1 - prev_disk.center 
     v2 = new_vert_2 - prev_disk.center 
 
-    # print prev_dir, v1
-    # print prev_dir, v2
     # Determine whether both segment vertices lie in the same half-plane.
     if np.dot(prev_disk.normal, v1) >= 0 and np.dot(prev_disk.normal, v2) >= 0.:
         # if so, return since no shifting is necessary.
@@ -204,8 +120,10 @@ def shift_new_disk(new_disk, prev_disk):
         # print "First one!"
         if in_same_half_plane(prev_dir, prev_disk.center, \
                                 new_vert_1, prev_vert_1):
+            # print "New_vert 1 and prev_vert 1 are in the same half-plane"
             new_disk = get_new_disk_points(prev_vert_1, new_vert_2, new_disk.normal)
         else:
+            # print "New_vert 1 and prev_vert 2 are in the same half-plane"
             new_disk = get_new_disk_points(prev_vert_2, new_vert_2, new_disk.normal)
 
     elif np.dot(prev_disk.normal, v2) < 0.:
@@ -237,8 +155,13 @@ def shift_new_disk(new_disk, prev_disk):
 
     # print prev_disk.normal, v2
     # print np.dot(prev_disk.normal, v2)
+    if abs(get_radius(new_disk.normal, new_disk.center, tunnel) - new_disk.radius) > 0.1:
+        new_disk.radius = get_radius(new_disk.normal, new_disk.center, tunnel)
+        return shift_new_disk(new_disk, prev_disk, tunnel)
 
-    assert np.dot(prev_disk.normal, v1) > -0.001 and np.dot(prev_disk.normal, v2) > -0.0001
+
+    print abs(get_radius(new_disk.normal, new_disk.center, tunnel) - new_disk.radius)
+    # assert np.dot(prev_disk.normal, v1) > -0.0001 and np.dot(prev_disk.normal, v2) > -0.0001
     return new_disk
 
 def is_follower(prev_disk, new_disk):
@@ -260,7 +183,7 @@ def load_tunnel_from_file(filename):
     for line in infile.readlines():
         words = line.split()
         #print words
-        if words[0] == "ATOM":
+        if len(words) > 0 and words[0] == "ATOM":
             center = np.array([float(words[6]), float(words[7]), float(words[8])])
             radius = float(words[9])
             tunnel.append(Sphere(center, radius));
@@ -274,19 +197,23 @@ def load_tunnel_from_file(filename):
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     tunnel = load_tunnel_from_file(arguments['<filename>'])
+    # tunnel = tunnel[3:7]
+    draw_ARG = arguments["--draw"]
 
     # draw tunnel
-    for i, s in enumerate(tunnel):
-      sVis = vs.sphere(pos = (s.center[0], s.center[1], s.center[2]), radius = s.radius, opacity=0.3)
-      # central line
-      if (i < len(tunnel)-1):
-        s2 = tunnel[i+1]
-        vVis = vs.arrow(pos=(s.center[0], s.center[1], s.center[2]), 
-                        axis=(s2.center[0]-s.center[0], s2.center[1]-s.center[1], s2.center[2]-s.center[2]), 
-                        color=(1,0,0), shaftwidth=1)
+    if draw_ARG:
+        for i, s in enumerate(tunnel):
+            sVis = vs.sphere(pos = (s.center[0], s.center[1], s.center[2]), 
+                                radius = s.radius, opacity=0.9)
+            # central line
+            if (i < len(tunnel)-1):
+                s2 = tunnel[i+1]
+                vVis = vs.arrow(pos=(s.center[0], s.center[1], s.center[2]), 
+                                axis=(s2.center[0]-s.center[0], s2.center[1]-s.center[1], s2.center[2]-s.center[2]), 
+                                color=(1,0,0), shaftwidth=1)
 
     delta = 0.1
-    eps   = delta / 10
+    eps   = delta / 10.
     disks = []
     centers = [s.center for s in tunnel]
     normals = [normalize(centers[i + 1] - centers[i]) for i in xrange(len(centers) - 1)]
@@ -318,7 +245,8 @@ if __name__ == '__main__':
                 if not is_follower(disks[-1], new_disk):
                     size += eps
                     continue
-                new_disk = shift_new_disk(new_disk, disks[-1])
+                print "Shift!--------------------------"
+                new_disk = shift_new_disk(new_disk, disks[-1], tunnel)
 
             if (len(disks) > 1 and disk_dist(new_disk, disks[-2]) < delta):
                 disks[-1] = new_disk
@@ -327,13 +255,25 @@ if __name__ == '__main__':
             size += eps
 
     # draw disks
-    for i, disk in enumerate(disks[:]):
-        if (i != 0):
-            print "Disk distance: {}".format(disk_dist(disks[i-1], disk))
-        vs.ring(pos=disk.center, 
-                axis=disk.normal, 
-                radius=disk.radius, 
-                thickness=0.01)
+    if draw_ARG:
+        for i, disk in enumerate(disks[:]):
+            if (i != 0):
+                print "Disk distance: {}".format(disk_dist(disks[i-1], disk))
+            vs.ring(pos=disk.center, 
+                    axis=disk.normal, 
+                    radius=disk.radius, 
+                    thickness=0.01,
+                    color=(1,0,0))
+
+        old = vector(scene.forward) # keep a copy of the old forward
+        while 1:
+            rate(50)
+            if scene.forward != old:
+                new = scene.forward
+                axis = cross(old,new)
+                angle = new.diff_angle(old)
+                lframe.rotate(axis=axis, angle=angle)
+                old = vector(new)
 
     disks_structured = [disk.to_dict() for disk in disks]
     # print json.dumps(disks_structured, sort_keys=True,