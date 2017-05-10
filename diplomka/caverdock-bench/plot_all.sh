73b30fff5e53272436b0f5cb9ca6109ec7c3de13
Jan Plhak <Jenda.Plhak@gmail.com>
2017-05-10 20:23:14 +0200

    Generate better images.

--- diplomka/caverdock-bench/plot_all.sh
+++ diplomka/caverdock-bench/plot_all.sh
@@ -1,7 +1,7 @@
 plot_in_folder () {
     for file in $1/*.dat; do
         base=$(basename "$file" .dat)
-        gnuplot -e "set terminal png size 1920, 1350; set output 'img/$1_${base}.png';set xlabel 'Ligand position';set ylabel 'Energy';plot '$1/${base}.dat' u 1:4 w l t 'covergence', '$1-noconv/${base}.dat' u 1:4 w l t 'orig';"
+        gnuplot -e "set terminal postscript eps enhanced color font 'Helvetica,18'; set output 'img/$1_${base}.eps';set xlabel 'Ligand position';set ylabel 'Energy';plot '$1/${base}.dat' u 1:4 w l lw 5 t 'covergence', '$1-noconv/${base}.dat' u 1:4 w l lw 5 t 'orig';"
         # gnuplot -e "set output 'img/$1_${base}.png'"
     done
 }
